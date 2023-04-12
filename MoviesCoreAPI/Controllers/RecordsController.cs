e00c75885a5759dd6e7df33d769cedd6fef246c0
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2023-04-12 13:26:31 +0200

    MoviesCoreAPI reposted...

---
+++ MoviesCoreAPI/Controllers/RecordsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using MoviesCoreAPI.Models;
+using MoviesCoreAPI.ViewModel;
+
+namespace MoviesCoreAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private MovieContext _context { get; set; }
+        private readonly IConfiguration _configuration;
+        public RecordsController(MovieContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: /records
+        [HttpGet]
+        public async Task<ActionResult<List<RecordsViewModel>>> GetAllRecords()
+        {
+            var records = await _context.Records.ToListAsync();
+            var recordsVMs = new List<RecordsViewModel>();
+
+            foreach (var record in records)
+            {
+                record.Movie = _context.Movies.Where(m => m.MovieID == record.MovieId).FirstOrDefault();
+                recordsVMs.Add(
+                    record
+                );
+            }
+
+            return recordsVMs;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecordsViewModel>> GetRecordByID(int id)
+        {
+            RecordsViewModel recordViewModel = await _context.Records.Where(r => r.RecordID == id).FirstOrDefaultAsync();
+            return recordViewModel;
+        }
+
+        // GET /records/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<RecordsViewModel>>> GetRecordsByUserId(int id)
+        {
+
+            var records = await _context.Records.Where(r=>r.UserId == id).ToListAsync();
+            var recordsVMs = new List<RecordsViewModel>();
+
+            foreach (var record in records)
+            {
+                recordsVMs.Add(
+                    new RecordsViewModel()
+                    {
+                        RecordID = record.RecordID,
+                        Rate = record.Rate,
+                        Movie = _context.Movies.Where(m => m.MovieID == record.MovieId).FirstOrDefault(),
+                        User = record.User
+                    }
+                );
+            }
+
+            return recordsVMs;
+        }
+
+        // POST /records
+        [HttpPost]
+        public async Task<HttpResponseMessage> PostRecords([FromBody]RecordPostDTO recordPostDTO)
+        {
+            string sqlCustomerInsert = $"INSERT INTO Records (Rate,MovieId,UserId) VALUES ({recordPostDTO.Rate},{recordPostDTO.MovieId},{recordPostDTO.UserId});";
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    var affectedRows = await connection.ExecuteAsync(sqlCustomerInsert);
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Radi misko!", System.Text.Encoding.UTF8, "application/json") };
+        }
+
+        //PUT /records/5
+        [HttpPut]
+        public async Task<IActionResult> PutRecord([FromBody] Records record)
+        {
+            //_context.Entry(record).State = EntityState.Modified;
+
+            _context.Records.Attach(record);
+            _context.Entry(record).Property(x => x.Rate).IsModified = true;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(GetRecordByID(record.RecordID) != null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE /records/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Records>> DeleteRecord(int id)
+        {
+            var record = await _context.Records.FindAsync(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            _context.Records.Remove(record);
+            var movie = await _context.Movies.FindAsync(record.MovieId);
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return record;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Moviium
- 
+
+# Angular10
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+kako pokreniti...
+api
+
+1. otvoriti Moviium.sln u visual studiu
+2. promjeniti putanju do baze u fajlovima MovieContext.cs i appsettings.json
+3. pokrenit migracije update-database
+4. pokrenit api
+
+angular
+
+1. otvoriti angular10 u visual studio codu
+2. promjenit api key... u angular10\src\app\omdb.service.ts private apiURL = 'http://www.omdbapi.com/?apikey={api_key}&';
+3. npm install pa ng serve
+4. http://localhost:4200/login
